b6c732b4b410258081d62a4dc9f19a592aa02791
BruhP8 <luc.biohazard@gmail.com>
2020-10-28 16:49:40 +0100

    Instance Header prototypes

--- Instance.h
+++ Instance.h
@@ -1,7 +1,47 @@
 #ifndef NETLIST_INSTANCE_H
 #define NETLIST_INSTANCE_H
 
-namespace Netlist{
+#include<string>
+#include<vector>
+#include"Cell.h"
+#include"Point.h"
 
+namespace Netlist{
+    class Instance
+    {
+    private:
+        Cell*               owner_;
+        Cell*               masterCell_;
+        std::string         name_;
+        std::vector<Term*>  terms_;
+        Point               position_;
+    public:
+        // CTOR
+                                            Instance(Cell* owner, Cell* model, const std::string&);
+        // DTOR
+                                            ~Instance();
+        // Accessors
+        inline const    std::string&        getName         () const;
+        inline          Cell*               getMasterCell   () const;
+        inline          Cell*               getCell         () const;
+        inline const    std::vector<Term*>& getTerms        () const;
+        inline          Term*               getTerm         (const std::string&) const;
+        inline          Point               getPosition     () const;
+        // Mutators
+                        bool                connect         (const std::string& name, Net*);
+                        void                add             (Term*);
+                        void                remove          (Term*);
+                        void                setPosition     (const Point&);
+                        void                setPosition     (int x, int y);
+    };
+    
+    Instance::Instance(Cell* owner, Cell* model, const std::string&)
+    {
+    }
+    
+    Instance::~Instance()
+    {
+    }
+    
 }
 #endif // NETLIST_INSTANCE_H
